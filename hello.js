6db96ea73add7593c67d40b95f9cc7038ed58885
Jenny <finej01@gmail.com>
2020-10-22 09:49:04 -0400

    add function code

---
+++ hello.js
@@ -0,0 +1,58 @@
+// function sayHello(name) {
+//     let msg = 'Hello, ' + name + '.How are you?';
+//     return msg;
+// }
+
+sayHello = (name) => {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+// sayHello = (name) => {
+//     let msg = 'Hello, ' + name + '. How are you?';
+//     return msg;
+// }
+
+// sayHello = (name) => {
+//     let msg = 'Hello, ' + name + '. How are you?';
+//     return msg;
+// }
+
+// sayHello = (name) => {
+//     let msg = 'Hello, ' + name + '. How are you?';
+//     return msg;
+// }
+
+// sayHello = (name) => {
+//     let msg = 'Hello, ' + name + '. How are you?';
+//     return msg;
+// }
+
+// sayHello = (name) => {
+//     let msg = 'Hello, ' + name + '. How are you?';
+//     return msg;
+// }
+
+// sayHello = (name) => {
+//     let msg = 'Hello, ' + name + '. How are you?';
+//     return msg;
+// }
+
+// sayHello = (name) => {
+//     let msg = 'Hello, ' + name + '. How are you?';
+//     return msg;
+// }
+
+// sayHello = (name) => {
+//     let msg = 'Hello, ' + name + '. How are you?';
+//     return msg;
+// }
+
+// sayHello = (name) => {
+//     let msg = 'Hello, ' + name + '. How are you?';
+//     return msg;
+// }
+
+
+console.log (sayHello('SEI 1019'));
+
